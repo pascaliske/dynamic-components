895fcd39704023c229f8ccfa52d1b8bdb10ea30a
Pascal Iske <info@pascal-iske.de>
2018-10-12 23:08:14 +0200

    fix(dynamic-components): fix reverse array mutation bug

--- projects/dynamic-components/src/lib/helpers.ts
+++ projects/dynamic-components/src/lib/helpers.ts
@@ -2,7 +2,7 @@
  * Converts the first letter of the given string to upper case.
  *
  * @param value - The string to be converted
- * @returns - The string with an upper cased first letter.
+ * @returns The string with an upper cased first letter.
  */
 export function upperFirst([first, ...rest]: string): string {
     return [first.toUpperCase(), ...rest].join('')
@@ -10,9 +10,11 @@ export function upperFirst([first, ...rest]: string): string {
 
 /**
  * Converts the given string to camel case.
- * @param value
+ *
+ * @param value - The non camel case string for conversion
+ * @returns The camel cased string
  */
-export function camelCase(value: string) {
+export function camelCase(value: string): string {
     const divider = (chars = ['-', '_', '.', ' ']) => {
         for (const char of chars) {
             if (value.includes(char)) {
@@ -24,4 +26,14 @@ export function camelCase(value: string) {
     return value.split(divider()).reduce((result, word, index) => {
         return result + (word ? (index ? upperFirst(word.toLowerCase()) : word.toLowerCase()) : '')
     }, '')
+}
+
+/**
+ * Reverses the given array without mutating it directly.
+ *
+ * @param array - The original array
+ * @returns The reversed version of the given array.
+ */
+export function reverse<T>(array: Array<T>): Array<T> {
+    return array.slice().reverse()
 }
